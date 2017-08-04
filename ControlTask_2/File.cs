88cf8446c725baf4dbeec329a6389e7d657835d7
dainek <dainek1993@gmail.com>
2017-08-04 20:59:54 +0300

    init

---
+++ ControlTask_2/File.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlTask_2
+{
+    class File
+    {
+        public File(string fileDescription)
+        {
+            string baseFileData = fileDescription.Split(';')[0];
+            Extension = baseFileData.Split('(')[0];
+            sizeStr = baseFileData.Split('(')[1].Split(')')[0];
+            Size = GetBytes();
+        }
+        public string Extension { get; private set; }
+        public int Size { get; private set; }
+        private string sizeStr;
+        public virtual void Print()
+        {
+            Console.WriteLine("Extension: " + Extension);
+            Console.WriteLine("Size: " + Size);
+        }
+
+        private int GetBytes()
+        {
+            int digit = 0;
+            char[] sizeChars = sizeStr.Split('B')[0].ToCharArray();
+            if (char.IsDigit(sizeChars[sizeChars.Length - 1]) == false)
+            {
+                sizeChars[sizeChars.Length - 1] = '\0';
+                
+            }
+            digit = int.Parse(new string(sizeChars));
+            switch (sizeStr[sizeStr.Length - 1])
+            {
+                case 'G':
+                    digit *= 1000000000;
+                    break;
+                case 'K':
+                    digit *= 1000;
+                    break;
+                case 'M':
+                    digit *= 1000000;
+                    break;
+            }
+
+            return digit;
+        }
+    }
+
+    class TextFile : File
+    {
+        public TextFile(string fileeDescription) : base(fileeDescription)
+        {
+            Content = fileeDescription.Split(';')[1];
+        }
+
+        public string Content { get; private set; }
+
+        public override void Print()
+        {
+            Console.WriteLine("TextFile:");
+            base.Print();
+            Console.WriteLine("Content: " + Content);
+        }
+    }
+
+    class Movie : File, IGraphicsFile
+    {
+        public Movie(string fileeDescription) : base(fileeDescription)
+        {
+            Resolution = fileeDescription.Split(';')[1];
+            Duration = fileeDescription.Split(';')[2];
+        }
+
+        public string Resolution { get;}
+        public string Duration { get; }
+
+        public override void Print()
+        {
+            Console.WriteLine("Movie:");
+           base.Print();
+            Console.WriteLine("Resolution: " + Resolution);
+            Console.WriteLine("Duration  " + Duration);
+        }
+    }
+
+    class Image : File, IGraphicsFile
+    {
+        public Image(string fileeDescription) : base(fileeDescription)
+        {
+            resolution = fileeDescription.Split(';')[1];
+        }
+        private string resolution;
+        public string Resolution
+        {
+            get
+            {
+                return resolution;   
+            }
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Image:");
+            base.Print();
+            Console.WriteLine("Resolution: " + Resolution);
+        }
+    }
+
+    interface IGraphicsFile
+    {
+        string Resolution { get;} 
+    }
+}
--- ControlTask_2/Program.cs
+++ ControlTask_2/Program.cs
@@ -10,6 +10,58 @@ namespace ControlTask_2
     {
         static void Main(string[] args)
         {
+            String text = @"Text: file.txt(6B); Some string content
+Image: img.bmp(19MB); 1920х1080
+Text:data.txt(12B); Another string
+Text:data1.txt(7B); Yet another string
+Movie:logan.2017.mkv(19GB); 1920х1080; 2h12m";
+
+            string[] filesData = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            File[] files = new File[filesData.Length];
+
+            for (int i = 0; i < filesData.Length; i++)
+            {
+                string fileDescription = filesData[i].Split(':')[1];
+                switch (filesData[i].Split(':')[0])
+                {
+                    case "Text":
+                        files[i] = new TextFile(fileDescription);
+                        break;
+                    case "Movie":
+                        files[i] = new Movie(fileDescription);
+                        break;
+                    case "Image":
+                        files[i] = new Image(fileDescription);
+                        break;
+                    default:
+                        files[i] = new File(fileDescription);
+                        break;
+                }
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i] is TextFile)
+                {
+                    files[i].Print();
+                }
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i] is Movie)
+                {
+                    files[i].Print();
+                }
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i] is Image)
+                {
+                    files[i].Print();
+                }
+            }
         }
     }
 }
